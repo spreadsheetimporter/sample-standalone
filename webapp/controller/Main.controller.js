049520c10fad7ee99da1d16ef9910afedd0a9d75
marianfoo <13335743+marianfoo@users.noreply.github.com>
2023-04-13 14:37:50 +0200

    add excel upload button

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,15 +1,22 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("exceluploadtotable.controller.Main", {
             onInit: function () {
+                var oTableModel = new JSONModel();
+                this.getView().setModel(oTableModel, "tableData");
+            },
 
+            uploadButtonPress(oEvent) {
+                const model = this.getView().getModel("tableData");
+                model.setData(oEvent.getParameter("payload"));
             }
         });
     });
